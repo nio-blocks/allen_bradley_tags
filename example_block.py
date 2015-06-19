f0383a48130e74511b57d63877a17e07d8f5b8e7
Chris Brackert <cbrackert@gmail.com>
2015-06-19 15:49:44 -0700

    Add version property to block

--- example_block.py
+++ example_block.py
@@ -1,11 +1,14 @@
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
+from nio.metadata.properties import VersionProperty
 
 
 @Discoverable(DiscoverableType.block)
 class Example(Block):
 
     """ This is the Example block. Put a brief description here. """
+
+    version = VersionProperty('0.1.0')
 
     def process_signals(self, signals, input_id='default'):
         for signal in signals:
